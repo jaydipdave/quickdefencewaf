93693e83f1ece63c3416be73c10aedbdee2e3d44
jaydipdave <jaydipdave@gmail.com>
2014-01-07 07:26:31 -0500

    Uploading filters

---
+++ Conf/header_filter.lua
@@ -0,0 +1,6 @@
+
+ngx.header['X-Powered-By'] = "JDWAF"
+ngx.header['X-AspNet-Version'] = nil
+ngx.header['Server'] = "Jenginx"
+ngx.header['X-Frame-Options'] = "deny"
+ngx.header['X-XSS-Protection'] = "1; mode=block"
